55a541479d422bbea2544e494e4886650b61832d
arefeh-crepuscule <arefeh.za1382@gmail.com>
2023-03-18 11:55:10 +0330

    setting state

--- src/Setting.java
+++ src/Setting.java
@@ -6,10 +6,20 @@ public class Setting {
     private final String address = "D:\\B - University\\AP - JAVA\\projects\\project-1\\tic-tac-toe project\\src\\new.txt";
 
 
+    /**
+     * a constructor that call readSetting function
+      * @param array an array include information
+     * @throws IOException
+     */
     public Setting(int[] array) throws IOException {
         readSetting(array);
     }
 
+    /**
+     * read saved information : side size , block cell , won condition , from txt file and save in an array
+      * @param info will include the information saved in txt file
+     * @throws IOException
+     */
     public void readSetting(int[] info) throws IOException {
 
         File file = new File(address);
@@ -21,6 +31,13 @@ public class Setting {
 
     }
 
+    /**
+     * chang the information in txt file with new user choice
+      * @param sideSize  new size of the board
+     * @param blockCell new number of cell that user want to block
+     * @param wonCondition new number of cell that need to win
+     * @throws IOException
+     */
     public void changeSetting(int sideSize, int blockCell, int wonCondition) throws IOException {
 
         Formatter formatter = new Formatter(address);
@@ -30,6 +47,10 @@ public class Setting {
 
     }
 
+    /**
+     * change back txt file  to default setting
+      * @throws IOException
+     */
     public void defaultSetting() throws IOException {
 
         Formatter formatter = new Formatter(address);
